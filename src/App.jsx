750e9ca8f995dc1aa8475c77321533c6fa4e8c1a
DavideCotigliani <davide.cotigliani@gmail.com>
2025-05-13 15:14:30 +0200

    [UPD] Mileston 3 completed, added a card for every actresses

--- src/App.jsx
+++ src/App.jsx
@@ -19,8 +19,25 @@ function App() {
     <>
       <ul className='card-group'>
         {actresses.map((actress) => (
-          <li key={actress.id} className='card-group-item'>{actress.name} {actress.birth_year}
-            {actress.nationality} {actress.biography} {actress.image} {actress.awards}</li>
+          <li key={actress.id} className='card-group-item'>
+            <div className="card rounded-0 d-flex flex-row">
+              <div className="col-5">
+                <div className="character-image">
+                  <img src={actress.image} alt={actress.name} className='img-fluid' />
+                </div>
+              </div>
+              <div className="col-7">
+                <div className="characters-infos d-flex flex-column justify-content-between p-3">
+                  <h3 className="actress-name">{actress.name}</h3>
+                  <p className='actress-birth'>{actress.birth_year}</p>
+                  <p className='actress-nationality'>{actress.nationality}</p>
+                  <p className='actress-biography'>{actress.biography}</p>
+                  <p className='actress-awards'>{actress.awards}</p>
+                  <p></p>
+                </div>
+              </div>
+            </div>
+          </li>
         ))}
       </ul>
     </>
@@ -28,3 +45,24 @@ function App() {
 }
 
 export default App
+
+
+
+{/* <div className="card rounded-0 d-flex flex-row">
+                <div className="col-5">
+                  <div className="character-image">
+                    <img
+                      src={character.image}
+                      alt={character.name}
+                      className="img-fluid"
+                    />
+                  </div>
+                </div>
+                <div className="col-7">
+                  <div className="character-infos d-flex flex-column justify-content-between p-3">
+                    <h3 className="character-name">{character.name}</h3>
+                    <p className="character-status">
+                      <span
+                        className={`status-icon is-${character.status.toLowerCase()}`}
+                      ></span>
+                      {character.status} */}
